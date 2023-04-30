c4411550735ee0386f9de3b37c84f5c6198e41e0
Zeeshan-Mehdi110 <Zeeshanmehdi253@gmail.com>
2023-04-30 16:27:58 +0500

    user module completed

--- controllers/users.js
+++ controllers/users.js
@@ -6,7 +6,7 @@ const { createJWTToken } = require("../utills/util");
 const { varifyUser } = require("../middlewares/user_auth");
 const router = express.Router()
 
-router.use(["/add","/edit","/delete"],varifyUser)
+router.use(["/add","/edit","/delete","/profile","/profile-update"],varifyUser)
 
 router.post("/add",async (req,res)=> {
   const userExist = await User.findOne({email : req.body.email})
@@ -81,6 +81,43 @@ router.delete("/delete",async (req,res) => {
     res.status(400).json({error : error.message})
   }
 
+})
+
+router.get("/profile",async (req,res) => {
+  try {
+    let user = await User.findById(req.user._id)
+    user = user.toObject()
+    delete user.password
+    res.json({user})
+  } catch (error) {
+    res.status(400).json({ error : error.message })
+  }
+})
+
+router.post("/profile-update",async (req,res)=> {
+  console.log(req.user)
+  const userExist = await User.findOne({email : req.body.email , _id : { $ne : req.user._id } })
+try {
+  if(userExist) throw new Error("This email is already registered")
+
+  const { name ,email,phone_number,profile_picture,password,type,active,created_on,modified_on } = req.body
+  let updatedUser =  await  User.findByIdAndUpdate(req.user._id ,{
+    name : name,
+    email : email,
+    phone_number,
+    profile_picture,
+    password : await bcrypt.hash(password , 10),
+    type,
+    active,
+    created_on :created_on,
+  }) 
+  await updatedUser.save();
+  updatedUser = updatedUser.toObject()
+  delete updatedUser.password
+  res.json({user : updatedUser})
+} catch (error) {
+  res.status(400).json({error : error.message})
+}
 })
 
 router.post("/signin",async (req,res) => {
