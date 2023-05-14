280a7264f22d507794546442aa72470b028607e4
Zeeshan-Mehdi110 <Zeeshanmehdi253@gmail.com>
2023-05-14 18:34:31 +0500

    Free Email Service Functionality on forgot Password

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,30 @@ import { loadAuth, signOut } from "./store/actions/authActions";
 import { connect } from "react-redux";
 import { Button } from "@mui/material";
 import AppPreLoader from "./components/library/AppPreLoader";
+import { Navigate, useLocation, useNavigate } from "react-router-dom";
+
+const publicRoutes = ["/","/admin/signin","/admin/forgot-password","/admin/reset-password/:resetCode"]
 
 function App({ user, isAuthLoaded, loadAuth }) {
+  const location = useLocation()
   useEffect(() => {
     loadAuth();
   }, [])
   
   if(!isAuthLoaded) return <AppPreLoader message={"Loading App ..."} />;
+  if(user && publicRoutes.includes(location.pathname))
+  return <Navigate to="/admin/dashboard" />
+  if(!user && !publicRoutes.includes(location.pathname))
+  return <Navigate to="/admin/signin" />
+
   if (!user) return <AppPublic />
   
-  return <div>
+  return (
+  <div>
     You are Signed in
     <Button onClick={signOut} >LogOut</Button>
   </div>
+  )
 }
 
 
--- client/src/components/auth/ForgotPassword.js
+++ client/src/components/auth/ForgotPassword.js
@@ -1,15 +1,24 @@
-import { Box, Button, Typography } from "@mui/material"
+import { Box, Button, CircularProgress, Typography } from "@mui/material"
 import { Field, Form } from "react-final-form"
 import TextInput from "../library/TextInput"
 import { Link } from "react-router-dom"
+import { useDispatch } from "react-redux"
+import axios from "axios"
+import { showError } from "../../store/actions/alertActions"
 
 const ForgotPassword = () => {
+  const dispatch = useDispatch()
   return (
     <Box  p={3} textAlign={"center"} bgcolor="#fff" boxShadow={'0px 0px 17px 5px #dbdada'} minWidth={"350px"} >
       <Typography textAlign={"center"} variant="h5" fontWeight={"bold"} pb={1} >Rate Me</Typography>
       <Form
         onSubmit={(data) => {
-          console.log(data)
+          return axios.post("/users/forgot-password",data).then(({data}) =>{
+
+          }).catch((err) => {
+            let message = err && err.response && err.response.data ? err.response.data.error : err.message;
+            dispatch(showError(message))
+          })
         }}
         validate={(data) => {
           const errors = {};
@@ -23,10 +32,11 @@ const ForgotPassword = () => {
       >
         {
           (props) => {
+            const { invalid , submitting }  = props;
             return (
               <form onSubmit={props.handleSubmit} action="">
-                <Field name="email" type="email" placeholder="Enter email address" component={TextInput} />
-                <Button type="submit" >Forgot Password</Button>
+                <Field name="email" type="email" placeholder="Enter email address" component={TextInput} autoFocus />
+                <Button type="submit" disabled={ submitting || invalid } >Reset Password { submitting && <CircularProgress size={20} style={{"marginLeft" : "10px"}} />  } </Button>
                 <Box mt={2} >
                   <Link style={{"textDecoration":"none"}} to="/admin/signin">Sign In</Link>
                 </Box>
