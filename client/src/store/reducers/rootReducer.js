6949e47ee32aaa435fa80323462a895a819eb3d5
Zeeshan-Mehdi110 <Zeeshanmehdi253@gmail.com>
2023-05-24 16:11:58 +0500

    compoleted  department module

--- client/src/store/reducers/rootReducer.js
+++ client/src/store/reducers/rootReducer.js
@@ -1,13 +1,15 @@
-import { combineReducers } from "redux";
-import alertReducer from "./alertReducer";
-import progressBarReducer from "./progressBarReducer";
-import authReducer from "./authReducer";
+import { combineReducers } from 'redux'
+import alertReducer from './alertReducer'
+import progressBarReducer from './progressBarReducer'
+import authReducer from './authReducer'
+import departmentReducer from './departmentReducer'
 
 const allReducers = {
   alert: alertReducer,
-  progressBar : progressBarReducer,
-  auth : authReducer,
+  progressBar: progressBarReducer,
+  auth: authReducer,
+  departments: departmentReducer
 }
 
-const rootReducers =  combineReducers(allReducers);
-export default rootReducers;
+const rootReducers = combineReducers(allReducers)
+export default rootReducers
--- models/Department.js
+++ models/Department.js
@@ -1,52 +1,49 @@
-const mongoose = require("mongoose")
-const moment = require("moment/moment")
+const mongoose = require('mongoose')
+const moment = require('moment/moment')
 
 const departmentSchema = new mongoose.Schema({
-  departmentName : {
-    type : String,
+  departmentName: {
+    type: String
   },
-  departmentEmail : {
-    type : String,
+  departmentEmail: {
+    type: String
   },
-  logo : {
-    type : String,
+  logo: {
+    type: String
   },
-  address : {
-    type : String,
+  address: {
+    type: String
   },
-  rating : {
-    type : Number
+  rating: {
+    type: Number
   },
-  userId : {
-    type : mongoose.Schema.Types.ObjectId
+  phoneNumber: {
+    type: Number
   },
-  phoneNumber : {
-    type : Number,
+  active: {
+    type: Number
   },
-  active : {
-    type : Number
+  departmentId: {
+    type: mongoose.Schema.Types.ObjectId
   },
-  departmentId : {
-    type : mongoose.Schema.Types.ObjectId
+  createdOn: {
+    type: Date,
+    default: moment().format('YYYY-MM-DD')
   },
-  createdOn : {
-    type : Date,
-    default : moment().format("YYYY-MM-DD")
-  },
-  modifiedOn : {
-    type : Date,
-    default : moment().format("YYYY-MM-DD")
-  },
+  modifiedOn: {
+    type: Date,
+    default: moment().format('YYYY-MM-DD')
+  }
 })
 departmentSchema.set('toJSON', {
   getters: true,
   transform: (doc, ret, options) => {
-    ret.createdOn = moment(ret.createdOn).format('YYYY-MM-DD');
-    ret.modifiedOn = moment(ret.modifiedOn).format('YYYY-MM-DD');
-    return ret;
+    ret.createdOn = moment(ret.createdOn).format('YYYY-MM-DD')
+    ret.modifiedOn = moment(ret.modifiedOn).format('YYYY-MM-DD')
+    return ret
   }
-});
+})
 
-const Department = mongoose.model("departments",departmentSchema)
+const Department = mongoose.model('departments', departmentSchema)
 
 module.exports = Department
