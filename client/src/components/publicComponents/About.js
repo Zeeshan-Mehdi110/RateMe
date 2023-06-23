aee10af7f3c35625f52741ab47343b69e461bbe4
Zeeshan-Mehdi110 <Zeeshanmehdi253@gmail.com>
2023-06-23 20:59:12 +0500

    Updated About.js

--- client/src/components/publicComponents/About.js
+++ client/src/components/publicComponents/About.js
@@ -1,4 +1,4 @@
-import { Box, Button, Grid, Typography } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 import Slider from "react-slick";
 import department1 from "../../static/state3.jpeg"
 const About = () => {
@@ -24,16 +24,16 @@ const About = () => {
   };
   return (
     <Slider {...settings} >
-      <Grid container sx={{ display: 'flex !important', 'backgroundColor': '#F2F0FF', "marginTop": "36px" }}>
+      <Grid container sx={{ display: 'flex !important', padding: { xs: "20px", sm: "0px" }, 'backgroundColor': '#F2F0FF', "marginTop": "36px" }}>
         <Grid item md={6} xs={12} sx={{ display: 'flex', textAlign: "center", 'alignItems': 'center' }} >
           <Box dipslay="flex" alignItems="center" >
             <Typography variant="subtitle1" component="div" sx={{ 'color': '#FB2E86', 'fontFamily': 'Lato', 'fontStyle': 'normal', 'fontWeight': '700', 'fontSize': '16px', 'lineHeight': '28px' }}>
               Users can give feedback by scanning a QR code.
             </Typography>
-            <Typography component="div" sx={{ 'fontFamily': 'var(--josefin)', "fontSize": { xs: "28px", md: "40px" }, 'fontWeight': 'bold', 'lineHeight': '70px', 'letterSpacing': '1.5' }}>
+            <Typography component="div" sx={{ 'fontFamily': 'Josef Sans', "fontSize": { xs: "28px", md: "40px" }, 'fontWeight': 'bold', 'lineHeight': '70px', 'letterSpacing': '1.5' }}>
               Feedback System for Government Departments
             </Typography>
-            <Typography my={3} component="div" variant="p" sx={{ 'color': '#8A8FB9', 'fontFamily': 'Lato', 'fontStyle': 'normal', 'fontWeight': '600', 'fontSize': '16px', 'lineHeight': '28px' }}>
+            <Typography my={3} component="div" sx={{ 'color': '#8A8FB9', 'fontFamily': 'Lato', 'fontStyle': 'normal', 'fontWeight': '600', 'fontSize': '16px', 'lineHeight': '28px' }}>
               This project is a feedback system that allows local users to give feedback on the services provided by government departments. The feedback is collected by scanning a QR.
             </Typography>
           </Box>
